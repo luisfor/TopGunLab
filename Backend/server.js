a717333a275dfd97587c4fa6a0038b8deeb8aeb4
luis eduardo canedo torres <luillys83@yahoo.es>
2022-07-23 22:47:46 -0500

    Configuracion para extraer todo a json

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 
 
@@ -13,6 +14,13 @@ app.use((req, res, next) => {
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
   });
+
+//middleware
+// convert what comes from requests to js
+app.use(bodyParser.urlencoded({ extended: false }));
+// convert request to json
+app.use(bodyParser.json());
+
 
 // Port run Server
 const PORT = process.env.PORT || 4000;
