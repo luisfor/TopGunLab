bf60ee44a30e81264662d21cfa4be199fa8a4191
luis eduardo canedo torres <luillys83@yahoo.es>
2022-07-23 22:40:36 -0500

    Realizando configuraciones de permiso de cors

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
+const cors = require("cors");
 
-const port = 4000;
 
-app.get('/', (req, res) => {
-    res.send(`Hola mundo top gun labs`);
-})
 
-app.listen(port, () => {
-    console.log(`Servidor On port ${port}`);
+//Cors
+// Config heads and cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+  });
+
+// Port run Server
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Server run port: ${PORT}`);
 })
